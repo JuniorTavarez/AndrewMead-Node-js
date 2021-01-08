9b1d3cb75e47dc6e9a97d6ccdd44efbab2e5b90d
juniortavarez <jtavarez8@gmail.com>
2021-01-08 11:50:43 -0500

    added file sync for practice

--- note-app/app.js
+++ note-app/app.js
@@ -1,3 +1,5 @@
 const fs = require('fs');
 
-fs.writeFileSync('note.txt', 'this file was created by node.js');
+fs.writeFileSync('note.txt', 'this file was created by node.js. ');
+
+fs.appendFileSync('note.txt', 'my name is john');
